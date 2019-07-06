827f7a709e72673bb50a7088e83b11a14278e4b5
ericksoen <michael.ericksen@gmail.com>
2019-07-05 22:07:58 -0500

    feat(lambda): add output variables for version and last modified

--- lambda/outputs.tf
+++ lambda/outputs.tf
@@ -5,3 +5,11 @@ output "lambda_arn" {
 output "invoke_arn" {
     value = "${aws_lambda_function.test_lambda.invoke_arn}"
 }
+
+output "lambda_version" {
+    value = "${aws_lambda_function.test_lambda.version}"
+}
+
+output "lambda_last_modified" {
+    value = "${aws_lambda_function.test_lambda.last_modified}"
+}
